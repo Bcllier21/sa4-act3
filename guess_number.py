2bc83de8446472401b453cfce3c4fcf5b2694879
Brian Collier <briancollier358@gmail.com>
2024-02-08 01:05:19 -0600

    Added conditionals for hints on if a number guess is too high or low.

--- guess_number.py
+++ guess_number.py
@@ -6,12 +6,17 @@ guess = int(input("What number am I thinking of? "))
 if guess == number:
     print("Congratulations! You guessed the right number.")
 elif guess != number:
+    print(f"Sorry! That's not the correct number.")
+    if guess < number: print("Too low of a guess.")
+    elif guess > number: print("Too high of a guess.")
     while guess != "q" or guess != number:
-        print(f"Sorry! That's not the correct number.")
         guess = input(f"You can guess again, or press 'q' to quit. ")
         if guess == str(number):
             print("Congratulations! You guessed the right number.")
             break
+        elif guess != str(number):
+            if int(guess) < number: print("Too low of a guess.")
+            elif int(guess) > number: print("Too high of a guess.")
         elif guess == "q":
             print(f"The correct number was actually {number}.")
             break
