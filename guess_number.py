0d576790e011385ccb7cfa8a55faad0f79315d62
Brian Collier <briancollier358@gmail.com>
2024-02-08 00:52:22 -0600

    Added while loop for repeat guesses and a quit option that reveals the correct number.

--- guess_number.py
+++ guess_number.py
@@ -5,5 +5,13 @@ guess = int(input("What number am I thinking of? "))
 
 if guess == number:
     print("Congratulations! You guessed the right number.")
-else:
-    print(f"Sorry! The number was {number}.")
+elif guess != number:
+    while guess != "q" or guess != number:
+        print(f"Sorry! That's not the correct number.")
+        guess = input(f"You can guess again, or press 'q' to quit. ")
+        if guess == str(number):
+            print("Congratulations! You guessed the right number.")
+            break
+        elif guess == "q":
+            print(f"The correct number was actually {number}.")
+            break
