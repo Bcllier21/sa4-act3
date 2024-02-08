4c0632a2b5f1a552396e5ddeb0bad3b8bb2653b8
Brian Collier <briancollier358@gmail.com>
2024-02-08 01:21:31 -0600

    Added limited guess system with warninings on how many are left before answer is revealed.

--- guess_number.py
+++ guess_number.py
@@ -6,12 +6,19 @@ guess = int(input("What number am I thinking of? "))
 if guess == number:
     print("Congratulations! You guessed the right number.")
 elif guess != number:
+    counter = 7        
+    print(f"Sorry! That's not the correct number. You get {counter} more guesses.")
     while guess != "q" or guess != number:
-        print(f"Sorry! That's not the correct number.")
         guess = input(f"You can guess again, or press 'q' to quit. ")
         if guess == str(number):
             print("Congratulations! You guessed the right number.")
             break
-        elif guess == "q":
+        elif guess != str(number) and counter == 2:
+            counter -= 1
+            print(f"Sorry! You have {counter} guess remaining before the answer's shown.")
+        elif guess != str(number) and counter > 1:
+            counter -= 1
+            print(f"Sorry! You have {counter} guesses remaining before the answer's shown.")
+        elif guess == "q" or counter == 1:
             print(f"The correct number was actually {number}.")
             break
